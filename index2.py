dc77083c016daa3cd8dae1e501d3f3603b30d76a
Daniel Park <hpark114@asu.edu>
2023-02-19 14:20:00 -0700

    Update index2.py
    Basic UI for Login and Create Account Pages (still needs work on positioning)
    Everything uses Grid now

--- index2.py
+++ index2.py
@@ -7,8 +7,8 @@ import tkinter.messagebox
 class CalorieCounterApp(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Calorie Counter App")
-        self.geometry("400x400")
+        self.title("Munch!")
+        self.geometry("360x420")
         self.logged_in = False
         self.username = ""
 
@@ -35,48 +35,54 @@ class CalorieCounterApp(tk.Tk):
 
 
         # Create login screen
-        self.login_frame = tk.Frame(self)
-        self.username_label = tk.Label(self.login_frame, text="Username:")
+        self.login_frame = tk.Frame(self, bg="beige")
+        self.welcome_label = tk.Label(self.login_frame, text="Munch!", fg="darkgreen", height=2, font=("Open Sans", 18), bg="beige")
+        self.welcome_label2 = tk.Label(self.login_frame, text="Track Your Daily Calories", height=2, bg="beige")
+
+        self.username_label = tk.Label(self.login_frame, text="Username:", bg="beige")
         self.username_entry = tk.Entry(self.login_frame)
-        self.password_label = tk.Label(self.login_frame, text="Password:")
+        self.password_label = tk.Label(self.login_frame, text="Password:", bg="beige")
         self.password_entry = tk.Entry(self.login_frame, show="*")
-        self.login_button = tk.Button(self.login_frame, text="Login", command=self.login)
-        self.Createaccount_button = tk.Button(self.login_frame, text="Create Account", command=self.tocreateaccount)
+        self.login_button = tk.Button(self.login_frame, text="Login", command=self.login, fg="darkgreen", height=2, width=5, bg="white", activebackground="beige")
+        self.Createaccount_button = tk.Button(self.login_frame, text="Create Account", command=self.tocreateaccount, fg="darkgreen", height=2, bg="white", activebackground="beige")
         
-        self.username_label.pack()
-        self.username_entry.pack()
-        self.password_label.pack()
-        self.password_entry.pack()
-        self.login_button.pack()
-        self.Createaccount_button.pack()
-        self.login_frame.pack()
+        self.welcome_label.grid(row=0, column=2)
+        self.welcome_label2.grid(row=2, column=2)
+        self.username_label.grid(row=4, column=2)
+        self.username_entry.grid(row=5, column=2)
+        self.password_label.grid(row=6, column=2)
+        self.password_entry.grid(row=7, column=2)
+        self.login_button.grid(row=8, column=2, sticky=W)
+        self.Createaccount_button.grid(row=8, column=2, sticky=E)
+        self.login_frame.grid(padx=100, pady=100)
 
 
         #Create create account screen
-        self.createAccount_frame = tk.Frame(self)
-        self.CreateUsername_label = tk.Label(self.createAccount_frame, text="Username:")
-        self.CreateUsername_entry = tk.Entry(self.createAccount_frame, textvariable=self.createUsername)
-        self.CreatePassword_label = tk.Label(self.createAccount_frame, text="Password:")
-        self.CreatePassword_entry = tk.Entry(self.createAccount_frame, show="*", textvariable=self.createPassword)
-        self.Createmail_label = tk.Label(self.createAccount_frame, text="Email: ")
-        self.Createmail_entry = tk.Entry(self.createAccount_frame,textvariable=self.createMail)
-        self.Createheight_label = tk.Label(self.createAccount_frame, text="Height")
-        self.Createheight_entry = tk.Entry(self.createAccount_frame, textvariable=self.createHeight)
-        self.Createweight_label = tk.Label(self.createAccount_frame, text="Weight:")
-        self.Createweight_entry = tk.Entry(self.createAccount_frame, textvariable=self.createWeight)
-        self.CreateAccount_button = tk.Button(self.createAccount_frame, text="Create Account", command=self.CreateAccount)
+        self.createAccount_frame = tk.Frame(self, background="beige")
+        self.CreateUsername_label = tk.Label(self.createAccount_frame, text="Username:", background="beige")
+        self.CreateUsername_entry = tk.Entry(self.createAccount_frame, textvariable=self.createUsername, justify=CENTER)
+        self.CreatePassword_label = tk.Label(self.createAccount_frame, text="Password:", background="beige")
+        self.CreatePassword_entry = tk.Entry(self.createAccount_frame, show="*", textvariable=self.createPassword, justify=CENTER)
+        self.Createmail_label = tk.Label(self.createAccount_frame, text="Email: ", background="beige")
+        self.Createmail_entry = tk.Entry(self.createAccount_frame,textvariable=self.createMail, justify=CENTER)
+        self.Createheight_label = tk.Label(self.createAccount_frame, text="Height (In Cm)", background="beige")
+        self.Createheight_entry = tk.Entry(self.createAccount_frame, textvariable=self.createHeight, justify=CENTER)
+        self.Createweight_label = tk.Label(self.createAccount_frame, text="Weight (In Kg):", background="beige")
+        self.Createweight_entry = tk.Entry(self.createAccount_frame, textvariable=self.createWeight, justify=CENTER)
+        self.CreateAccount_button = tk.Button(self.createAccount_frame, text="Create Account", command=self.CreateAccount, background="white", activebackground="beige", fg="darkgreen")
 
-        self.CreateUsername_label.pack()
-        self.CreateUsername_entry.pack()
-        self.CreatePassword_label.pack()
-        self.CreatePassword_entry.pack()
-        self.Createmail_label.pack()
-        self.Createmail_entry.pack()
-        self.Createheight_label.pack()
-        self.Createheight_entry.pack()
-        self.Createweight_label.pack()
-        self.Createweight_entry.pack()
-        self.CreateAccount_button.pack()
+        self.CreateUsername_label.grid(column=1)
+        self.CreateUsername_entry.grid(column=1)
+        self.CreatePassword_label.grid(column=1)
+        self.CreatePassword_entry.grid(column=1)
+        self.Createmail_label.grid(column=1)
+        self.Createmail_entry.grid(column=1)
+        self.Createheight_label.grid(column=1)
+        self.Createheight_entry.grid(column=1)
+        self.Createweight_label.grid(column=1)
+        self.Createweight_entry.grid(column=1)
+        self.CreateAccount_button.grid(column=1)
+        self.createAccount_frame.grid(padx=100, pady=100)
 
 
 
@@ -92,13 +98,13 @@ class CalorieCounterApp(tk.Tk):
         self.progress_bar = tk.Canvas(self.calorie_counter_frame, width=300, height=20)
         self.logout_button = tk.Button(self.calorie_counter_frame, text="Logout", command=self.logout)
         
-        self.food_label.pack()
-        self.food_entry.pack()
-        self.calories_label.pack()
-        self.calories_entry.pack()
-        self.log_button.pack()
-        self.progress_bar.pack()
-        self.logout_button.pack()
+        self.food_label.grid()
+        self.food_entry.grid()
+        self.calories_label.grid()
+        self.calories_entry.grid()
+        self.log_button.grid()
+        self.progress_bar.grid()
+        self.logout_button.grid()
         
     def login(self):
         with open("users.json") as f:
@@ -111,19 +117,19 @@ class CalorieCounterApp(tk.Tk):
         if username in users and password == users[username]:
             self.logged_in = True
             self.username = username
-            self.login_frame.pack_forget() # hide the login screen
-            self.calorie_counter_frame.pack() # show the calorie counter screen
+            self.login_frame.grid_forget() # hide the login screen
+            self.calorie_counter_frame.grid() # show the calorie counter screen
             self.update_progress_bar() # initialize the progress bar
         else:
             # Display an error message if the username or password is incorrect
-            error_label = tk.Label(self.login_frame, text="Incorrect username or password")
-            error_label.pack()
+            error_label = tk.Label(self.login_frame, text="Incorrect username or password", font=("Open Sans", 8), bg="beige")
+            error_label.grid(row=10, column=1)
     
     def logout(self):
         self.logged_in = False
         self.username = ""
-        self.calorie_counter_frame.pack_forget() # hide the calorie counter screen
-        self.login_frame.pack() # show the login screen
+        self.calorie_counter_frame.grid_forget() # hide the calorie counter screen
+        self.login_frame.grid() # show the login screen
         
     def log_calories(self):
         pass
@@ -147,8 +153,8 @@ class CalorieCounterApp(tk.Tk):
 
     def tocreateaccount(self):
         #shows the create account page
-        self.login_frame.pack_forget()
-        self.createAccount_frame.pack()
+        self.login_frame.grid_forget()
+        self.createAccount_frame.grid()
 
     def CreateAccount(self):
         #saves users credentials and information in the json file
